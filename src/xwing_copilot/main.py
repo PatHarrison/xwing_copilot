df3642d68f9abd29d84cd4df2122998546e319eb
PatHarrison <patrickgharrison@outlook.com>
2022-11-01 14:53:47 -0600

    remove other readme.rst

--- src/xwing_copilot/main.py
+++ src/xwing_copilot/main.py
@@ -1,11 +1,12 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 
 class XwingCopilot(App):
     def build(self):
-        self.icon = "assests/icon.png"
-        return Label(text="X-Wing Copilot")
+        self.icon = r"assests/icon.png"
+        sm = ScreenManager(transition=NoTransition())
+        return sm
 
 
 if __name__ == "__main__":
